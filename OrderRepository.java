40e17b497ef5c8729861b9bced6811a6a5ec355a
sunitha <gundarapuramana@gmail.com>
2026-01-06 14:32:28 +0530

    Add files via upload

---
+++ OrderRepository.java
@@ -0,0 +1,11 @@
+package com.klcanteen.repository;
+
+import com.klcanteen.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    Optional<Order> findByOrderCode(String orderCode);
+}
