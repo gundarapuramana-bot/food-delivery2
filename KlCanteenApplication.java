ff5e5d0e5d9402218d1ad125b3a7b5210bac2eee
sunitha <gundarapuramana@gmail.com>
2026-01-06 14:30:02 +0530

    Add files via upload

---
+++ KlCanteenApplication.java
@@ -0,0 +1,12 @@
+package com.klcanteen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlCanteenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KlCanteenApplication.class, args);
+    }
+}
